9ea216466d171e0d1f21271a7b195e2ed96b1471
pornima31 <pornima31hadap@gmail.com>
2023-01-22 00:40:32 +0530

    addition

---
+++ src/com/velocity/Array.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+public class Array {
+	public static void main(String[] args) {
+		int num = 1234;
+		int sum =0;
+		while (num>0) {
+			sum = sum + num/10;
+			num = num/10;
+		}
+	System.out.println("sum of digit in number :" +sum);
+}
+
+}
