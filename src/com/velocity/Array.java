ab272602304658fa8dd436b599704f9c71704743
pornima31 <pornima31hadap@gmail.com>
2023-01-22 14:38:26 +0530

    hello

--- src/com/velocity/Array.java
+++ src/com/velocity/Array.java
@@ -2,7 +2,7 @@ package com.velocity;
 
 public class Array {
 	public static void main(String[] args) {
-		int num = 1234;
+		int num = 234567;
 		int sum =0;
 		while (num>0) {
 			sum = sum + num/10;
---
+++ src/com/velocity/Array2.java
@@ -0,0 +1,10 @@
+package com.velocity;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to java");
+
+	}
+
+}
